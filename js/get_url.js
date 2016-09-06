ed475b4861d9e3acbaa5fe155276ecd37ff2e627
Redim <1153710218@stu.hit.edu.cn>
2016-09-06 20:52:22 +0800

    Add files via upload

--- js/get_url.js
+++ js/get_url.js
@@ -1,11 +1,9 @@
-function get_url(elem)
-{
-	chrome.runtime.sendMessage('Hello', function(response)
-	{
-		elem.innerHTML = response;
-	});
-	setTimeout(function(){get_url(elem)}, 1000);
-}
-
 var idiv = document.getElementById('url_div');
-get_url(idiv);
+var curTab
+//var selTabs;
+chrome.tabs.query({'highlighted': true, 'currentWindow': true}, function(tabs){
+	//selTabs=tabs;
+	curTab=tabs[0];
+	idiv.innerHTML=curTab.url;
+});
+
