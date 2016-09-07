383c1932072ee64d70954a449f2bf7b059b74068
Redim <1153710218@stu.hit.edu.cn>
2016-09-07 21:54:08 +0800

    version 1.0.0.5

--- js/status.js
+++ js/status.js
@@ -5,37 +5,35 @@ function Mes(url, title) {
 }
 
 function update() {
-
 	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 		/*console.log(tab.url + "  " + changeInfo.status);*/
-		if (tab.status == "complete" && tab.url != "chrome://newtab/") {
+		if (tab.status == "loading" && tab.url != "chrome://newtab/") {
 			if (!TabMes[tab.id]) {
-				console.log(now_url + "-->" + tab.url + "\n" + now_title + "-->" + tab.title);
+				console.log(curPage.url + "-->" + tab.url + "\n" + curPage.title + "-->" + tab.title);
 				TabMes[tab.id] = new Mes(tab.url, tab.title);
 			} else if (TabMes[tab.id].url!=tab.url){
 				console.log(TabMes[tab.id].url + "-->" + tab.url + "\n" + TabMes[tab.id].title + "-->" + tab.title);
 				TabMes[tab.id] = new Mes(tab.url, tab.title);
 			}			
-			/*
-				alert(now_url + "-->" + tab.url + "\n" + now_title + "-->" + tab.title); /// add an edge from (now_url -> tab.url) in storage
-			
-				if (tab.highlighted == true) {
-				//last_title = now_title;
-				//last_url = now_url;
-				now_title = tab.title;
-				now_url = tab.url;
-			}
-			*/
+			flag=false;
+			chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs){
+				curPage={url : tabs[0].url, title : tabs[0].title};
+			})
 		}
+	});
+
+	//在新标签页打开网页时打上标记，以免随后的onActived事件改变curPage
+	chrome.tabs.onCreated.addListener(function(tab){
+		if (tab.url!="chrome://newtab/") flag=true;
 	});
 
 	//在切换页面时更新当前标签页的信息
 	chrome.tabs.onActivated.addListener(function (activeInfo) {
 		Id = activeInfo.tabId;
 		chrome.tabs.get(Id, function (tab) {
-			if (tab.status=="complete"){
-			now_title = tab.title;
-			now_url = tab.url;
+			if (!flag){
+				curPage.title=tab.title;
+				curPage.url=tab.url;
 			}
 		});
 	});
@@ -43,5 +41,5 @@ function update() {
 }
 
 var curPage=new Mes("about:blank","Blank"),TabMes = [];
-
+var flag=false;	//当前网页是否为正在载入的
 update();
